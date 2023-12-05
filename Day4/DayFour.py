41ec60915d6de4f15b042eae9c0020bbc2afe670
Daniel Badura <daniel.badura@outlook.com>
2023-12-05 19:22:20 +0100

    Day 4

---
+++ Day4/DayFour.py
@@ -0,0 +1,28 @@
+with open('data.txt', 'r') as file:
+    card_lines = file.readlines()
+
+total_points = 0
+total_scratch_cards = 0
+line_copies_dict = {i: 1 for i in range(len(card_lines))}
+
+for line_number, card_line in enumerate(card_lines):
+    semi_colon_idx = card_line.index(':')
+    bar_idx = card_line.index('|')
+
+    winning_numbers = [num for num in card_line[semi_colon_idx:bar_idx].split() if num.isdigit()]
+    own_numbers = [num for num in card_line[bar_idx:-1].split() if num.isdigit()]
+    matching_numbers = [x for x in winning_numbers if x in own_numbers]
+    matching_numbers_len = len(matching_numbers)
+
+    total_scratch_cards += 1
+    scratch_copies = line_copies_dict[line_number]
+
+    if matching_numbers_len > 0:
+        total_points += 2 ** (matching_numbers_len - 1)
+        total_scratch_cards += (matching_numbers_len * scratch_copies)
+
+    for i in range(line_number, line_number + matching_numbers_len):
+        line_copies_dict[i + 1] = line_copies_dict.get(i + 1, 0) + (1 * scratch_copies)
+
+print('Day 4 part 1 answer:', total_points)
+print('Day 4 part 2 answer:', total_scratch_cards)
