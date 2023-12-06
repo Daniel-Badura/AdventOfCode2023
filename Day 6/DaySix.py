17ec50275c182dc5d319f012d1289bc2cf2d4eb3
Daniel Badura <daniel.badura@outlook.com>
2023-12-06 12:56:01 +0100

    Day 6 pt 2

--- Day 6/DaySix.py
+++ Day 6/DaySix.py
@@ -6,24 +6,29 @@ with open("data.txt", "r") as file:
     _, distance = datas[1].split(": ")
     times = list(map(int, time.split()))
     distances = list(map(int, distance.split()))
-
-    print(times)
-    print(distances)
+    print(distance[1].replace(" ", ""))
+    big_time = [int(time.replace(" ", ""))]
+    big_distance = [int(distance.replace(" ", ""))]
 # create resulting variables
-    possible = 0
-    possibles = []
+    
     #  loop through the races
-    for time in times:
-        # check all possible outcomes
-        for i in range(1,time+1):
-           if((i * (time - i))>distances[times.index(time)]):
-               possible += 1
-        else:
-            possibles.append(possible)
-            possible = 0
-    multiplication = 1
-    # multiply the possibilities
-    for race in possibles:
-        multiplication *= race
-    print(possibles)
-    print(multiplication)
+    def calculate(times_list, distance_list):
+        possible = 0
+        possibles = []
+        for time in times_list:
+            # check all possible outcomes
+            for i in range(1,time+1):
+                if((i * (time - i))>distance_list[times_list.index(time)]):
+                    possible += 1
+            else:
+                possibles.append(possible)
+                possible = 0
+        multiplication = 1
+        # multiply the possibilities
+        for race in possibles:
+            multiplication *= race
+        print(possibles)
+        print(multiplication)
+
+    calculate(times, distances)
+    calculate(big_time, big_distance)
